de1894cae6800e05e92de7d8a0d1cf6cc15cdc09
ebisuke <fungi@rect.sakura.ne.jp>
2021-02-18 04:49:36 +0900

    First Commit

---
+++ tosspc/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+namespace tosspc
+{
+    internal class Program
+    {
+        [DllImport("user32.dll")]
+        static extern IntPtr GetActiveWindow();
+        public static void Main(string[] args)
+        {
+            Process tos = Process.Start(args[2]);
+            while (!tos.HasExited)
+            {
+
+                if (GetActiveWindow() == tos.MainWindowHandle)
+                {
+                    SendKeys.Send(" ");
+                }
+                Thread.Sleep(100);
+            }
+            
+        }
+    }
+}
